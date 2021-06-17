acca424839a5722fe6f55d4e554faa5e6d5c606f
mikewojnowicz <mwojnowi@uci.edu>
2021-06-16 20:04:25 -0700

    Added test of arithmetic module using random numbers.

--- tests/unit/test_arithmetic.py
+++ tests/unit/test_arithmetic.py
@@ -1,4 +1,5 @@
 import pytest  # noqa
+import random 
 
 from acc.arithmetic import  add_numbers_incorrectly
 
@@ -6,3 +7,9 @@ def test_add_numbers_incorrectly():
     assert add_numbers_incorrectly(2,0)==2
     assert add_numbers_incorrectly(2,1)==3
 
+def test_add_numbers_incorrectly_with_random_numbers():
+    random.seed()
+    x=random.uniform(0, 1)
+    y=random.uniform(0, 1)
+    expected_result = x+y 
+    assert add_numbers_incorrectly(x,y)==expected_result 
