178e0179e6ea99c01a9ffee5d67546d6557010a6
mikewojnowicz <mwojnowi@uci.edu>
2021-06-18 11:32:42 -0700

    Added format violations and syntax violations.

---
+++ pre-fixes/format_violations_pre_fix.py
@@ -0,0 +1,6 @@
+# fmt: off
+variable_declaration_with_no_space_after_equal_sign=2
+
+def function_with_really_long_line_in_body(x: float) -> float:
+    return (x + 1 - x + 2 + x + 3 - x + 4) + (x + 5 - x + 6 + x + 7 - x + 8) + (x + 9 - x + 10 + x + 11 - x + 12) + (x + 13 - x + 14 + x + 15 - x + 16)
+# fmt: on
---
+++ src/acc/syntax_violations.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def this_function_has_an_undeclared_variable():
+    np.sum(x / 2)
