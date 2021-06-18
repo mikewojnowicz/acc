2ce9f904f310f5df73c40d4b53aa0cc4a7bcccc7
mikewojnowicz <mwojnowi@uci.edu>
2021-06-17 23:21:29 -0700

    Fix import statements for catmath.

--- tests/unit/test_catmath.py
+++ tests/unit/test_catmath.py
@@ -1,4 +1,4 @@
-from catinabox import catmath
+from acc.catmath import cat_years_to_hooman_years, is_cat_leap_year  # noqa
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
@@ -15,5 +15,6 @@ def test__cat_years_to_hooman_years__0__returns_0():
 
 # BONUS MATERIAL FOR STEP 2
 
+
 def test__is_cat_leap_year__succeeds():
-    assert catmath.is_cat_leap_year(2016) is True
+    assert is_cat_leap_year(2016) is True
